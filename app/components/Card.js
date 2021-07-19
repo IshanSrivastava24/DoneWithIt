fa5196e4abb2b691a430f1c0bf423fa74f0dd230
Ishan Srivastava <ishan05278@gmail.com>
2021-07-19 15:40:22 +0530

    ListItem Done, ViewImageScreen done

--- app/components/Card.js
+++ app/components/Card.js
@@ -10,8 +10,10 @@ function Card({title , subTitle , image}) {
         style={styles.card}
         >
             <Image style={styles.image} source={image} />
-            <AppText>{title}</AppText>
-            <AppText>{subTitle}</AppText> 
+            <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText> 
+            </View>
         </View>
     );
 }
@@ -21,10 +23,23 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         backgroundColor: colors.white,
         marginBottom: 20 ,
+        overflow: "hidden",
     },
     image: {
+        width: "100%",
+        height: 200,
+    },
+    detailsContainer: {
+        padding: 20,
 
-    }
+    },
+    title: {
+        marginBottom: 7,
+    },
+    subTitle: {
+        color: colors.secondary,
+        fontWeight: "bold",
+    },
 })
 
 export default Card;
