17cf0be324a9e916edf7e7238bcf9b2f23db6fbc
Ishan Srivastava <ishan05278@gmail.com>
2021-07-20 18:59:29 +0530

    AppTextInput Added

--- App.js
+++ App.js
@@ -10,11 +10,14 @@ import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screen/AccountScreen';
 import ListingScreen from './app/screen/ListingScreen';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
   
   return (
-    <ListingScreen />
+    <Screen>
+      <AppTextInput placeholder= "Username " icon = 'email' />
+    </Screen>
   ); 
 }
 
